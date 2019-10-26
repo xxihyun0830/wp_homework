137f34cbe478b6bfa5aac6aea18055e8ebf4dc3f
xxihyun0830 <xxihyun0830@gmail.com>
2019-10-27 08:17:45 +0900

    wp_hw04 sort.js

---
+++ wp_HW04/sort.js
@@ -0,0 +1,94 @@
+"use strict";
+//60181660 이지현
+
+function swap(arr, i, j) {
+  var t = arr[i];
+  arr[i] = arr[j];
+  arr[j] = t;
+ 
+}
+
+/* 필요에 따라 추가 함수 구현 가능 */
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
+
+function partition_point(arr, left, right){
+  var r  = getRandomInt(left, right);
+  swap(arr, left, r);
+  var pivot = left;
+  var store = left + 1;
+
+  for (var i=left+1; i<right+1; i++) {
+    if( arr[i] < arr[pivot]) {
+      swap(arr,i, store);
+      store += 1;
+    }
+  }
+  swap(arr, pivot, store-1);
+  return store-1;  
+}
+
+
+function quick_partition_sort(arr, left, right){
+  if (left >= right){
+    return ;
+  }
+  var pivot = partition_point(arr,left, right);
+  quick_partition_sort(arr, left, pivot-1);
+  quick_partition_sort(arr, pivot+1, right);
+}
+
+function quickSort(arr) {
+  /* 구현 필요 */
+  quick_partition_sort(arr, 0, (arr.length)-1);
+  return arr;
+}
+
+var data = [
+  [6, 3, 2, 7, 8, 3, 1, 0, 9, 5],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+  [1, 10, 2, 21],
+  [],
+  [1,1,1,1,1,1,1,1,1,1]
+];
+
+// 두 배열 a와 b의 내용을 비교. 같으면 true 아니면 false
+function compareArray(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  }
+  for(var i in a) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//배열 a의 각 원소를 복사하여 새로운 배열을 return
+function deepCopyArray(a) {
+  var ret = [];
+  for(var i in a) {
+    ret[i] = a[i];
+  }
+  return ret;
+}
+
+// 주어진 data에 대해 JavaScript Array.sort함수의 결과와 비교하여 다르면 에러 출력
+for(var i in data) {
+  var arr1 = deepCopyArray(data[i]);
+  var arr2 = deepCopyArray(data[i]);
+  quickSort(arr1);
+  arr2.sort(function(a, b) {return a-b;});
+  if (!compareArray(arr1, arr2)) {
+    console.log("Fail!!!", data[i], arr1, arr2);
+  }
+  
+  // else{
+  //   console.log("success -> ", data[i],"copied arr : ",arr1,"sorted arr : ",arr2)
+  // }
+}
